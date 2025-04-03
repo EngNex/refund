5f7008dbf28b7c1604695481b18103e1b77b11b4
EngNex <mecaunip2015@gmail.com>
2025-04-03 13:06:16 -0300

    populating the list with images

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+//Seleciona os elementos da lista
+
+const expenseList = document.querySelector("ul")
+
 
 //adiciona um evento de input para limpar o input ao digitar e deixar somente numeros
 amount.oninput = () => {
@@ -35,12 +39,38 @@ form.onsubmit = (event) => {
     const newExpense = {
         id: new Date().getTime(),
         expense: expense.value,
-        category: category.value,
+        category_id: category.value,
         category_name: category.options[category.selectedIndex].text,
         amount: amount.value,
         created_at: new Date(),
 
         } 
 
+        //chama a função que ira adicionar o item na lista
+        expenseAdd(newExpense)
     }
 
+function expenseAdd(newExpense) {
+try {  
+//cria o elemento de li para adiconar os itens na lista (ul)
+    const expenseItem = document.createElement("li")
+
+    //adiciona os dados do novo item na lista
+    expenseItem.classList.add("expense")
+
+    //cria o elemento visual de imagem relacionado ao tipo de consumo
+   const expenseIcon =  document.createElement("img")
+
+   expenseIcon.setAttribute("src",`img/${newExpense.category_id}.svg`)
+   expenseIcon.setAttribute("alt", newExpense.category_name)
+    
+// Adicona as informações no item
+   expenseItem.append(expenseIcon)
+   expenseList.append(expenseItem)
+
+}catch (e) {
+    alert("Não foi possivel atualizar a lista de despesas.")
+    console.error('Error:', e)
+    }
+}
+    
