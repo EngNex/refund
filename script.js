3ceabd0c1a42b25d4155dfd84bdb23df37024630
EngNex <mecaunip2015@gmail.com>
2025-04-03 12:37:01 -0300

    capturing the values ​​from the form

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //seleciona os elementos do formulario
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
 
 //adiciona um evento de input para limpar o input ao digitar e deixar somente numeros
 amount.oninput = () => {
@@ -21,4 +25,22 @@ function formatCurrenciesBRL(value){
     })
     return value
 }
+//#f402 aula
+//Captura o evento de submit do form para obter os valores
+form.onsubmit = (event) => {
+    //previne o formulario de ser enviado normalmente
+    event.preventDefault() 
+
+    //cria um novo objeto com os dados do formulario
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+
+        } 
+
+    }
 
